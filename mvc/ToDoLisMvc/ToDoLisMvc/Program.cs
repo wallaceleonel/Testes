fc521c2545d31d40231fd2763ca3a6c3b832612d
wallaceleonel <wallace.leonell@outlook.com>
2022-07-23 12:47:55 -0300

    corração do erro " Main"

--- mvc/ToDoLisMvc/ToDoLisMvc/Program.cs
+++ mvc/ToDoLisMvc/ToDoLisMvc/Program.cs
@@ -1,15 +1,5 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using ToDoListApi.Models;
 
 namespace ToDoLisMvc
@@ -21,11 +11,22 @@ namespace ToDoLisMvc
         {
             Configuration = configuration;
         }
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void  ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<_DbContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")
             ));
