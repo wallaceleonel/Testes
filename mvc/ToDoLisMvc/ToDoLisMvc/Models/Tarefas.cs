4431c7cd5877cc1ebdcbc4eccd0849c3d3416919
wallaceleonel <wallace.leonell@outlook.com>
2022-07-23 02:54:48 -0300

    mvc

---
+++ mvc/ToDoLisMvc/ToDoLisMvc/Models/Tarefas.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ToDoListApi.Models
+{
+    [Table(name: "Tarefas")]
+    public class Tarefas
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(120)]
+        public string Titulo { get; set; }
+        [MaxLength(500)]
+        public string Detalhes { get; set; }
+        public DateTime DateTarefa { get; set; }
+    }
+}
---
+++ mvc/ToDoLisMvc/ToDoLisMvc/Pages/Index.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.TeamFoundation.Dashboards.WebApi;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
+
+namespace ToDoLisMvc.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private IConfiguration Configuration;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+
+
+        [BindProperty]
+        public Dashboard Dashboard { get; set; }
+
+        string BaseUrl => Configuration.GetConnectionString("ApiUrl");
+
+        public async Task OnGetAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/Tarefas/");
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = response.Content.ReadAsStringAsync().Result;
+                    Dashboard = JsonConvert.DeserializeObject<Dashboard>(result);
+                }
+            }
+        }
+    }
+}
