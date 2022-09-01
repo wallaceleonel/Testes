9851b000f92e04d2c94465ef36702e5b7442c4a6
wallaceleonel <wallace.leonell@outlook.com>
2022-09-01 19:48:59 -0300

    criação do modelo base e construção da model view e controller

---
+++ CodeChanlange/Tintas.net/Controllers/CalculoController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Tintas.net.Models;
+
+namespace Tintas.net.Controllers
+{
+    public class CalculoController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ComodoModel comodoModel)
+        {
+            // Janela 
+            var AreaJanela = 2.00 * 1.20;
+            var Janela = comodoModel.QtdJanela * AreaJanela;
+            
+            // Porta 
+            var AreaPorta = 0.80 * 1.90;
+            var Porta = comodoModel.QtdPorta * AreaPorta;
+
+            // calculo da area de cada parede.
+            var ParedeFrente = comodoModel.LarguraParedeFrente * comodoModel.AltruaParedeFrente;
+            var ParedeFundo = comodoModel.LarguraParedeFundo * comodoModel.AlturaParedeFundo;
+            var ParedeEsqueda = comodoModel.LarguraParedeEsquerda * comodoModel.AlturaParedeEsquerda;
+            var ParedeDireita = comodoModel.LarguraParedeDireita * comodoModel.AlturaParedeDireita;
+
+            // calculo da area do comodo.
+            comodoModel.ConsumoTinta = ParedeFundo + ParedeFrente + ParedeDireita + ParedeEsqueda;
+            
+            return View(comodoModel.ConsumoTinta);
+        }
+    }
+}
---
+++ CodeChanlange/Tintas.net/Models/ComodoModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tintas.net.Models
+{
+    public class ComodoModel
+    {
+        [Required]
+        [Display(Name= "Parede Fundo")]
+        public int AlturaParedeFundo{ get; set; }
+        public int LarguraParedeFundo { get; set; }
+        [Required]
+        [Display(Name = "Parede Frente")]
+        public int AltruaParedeFrente { get; set; }
+        public int LarguraParedeFrente { get; set; }
+        [Required]
+        [Display(Name = "Parede Esquerda")]
+        public int AlturaParedeEsquerda { get; set; }
+        public int LarguraParedeEsquerda { get; set; }
+        [Required]
+        [Display(Name = "Parede Direita")]
+        public int AlturaParedeDireita { get; set; }
+        public int LarguraParedeDireita { get; set; }
+    
+        public int QtdJanela { get; set; }
+        public int QtdPorta { get; set; }
+
+        /// <summary>
+        /// consumo total de tinda no comodo.
+        /// </summary>
+        ///
+
+        [Display(Name= "Consumo")]
+        public int ConsumoTinta { get; set; }
+    }
+}
